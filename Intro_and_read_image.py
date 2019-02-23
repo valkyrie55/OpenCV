ddecac282052e23c0b3bbeca9ed60ae6d52d517d
Shweta <38096594+valkyrie55@users.noreply.github.com>
2019-02-23 19:30:40 +0000

    Create Intro_and_read_image.py

---
+++ Intro_and_read_image.py
@@ -0,0 +1,9 @@
+# OpenCV is an image and video processing library for all sort of image and video analysis.
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img = cv2.imread(r'C:\Users\UPES\Pictures\pic1.jpg', cv2.IMREAD_GRAYSCALE)
+plt.imshow(img,cmap = 'gray' )
+plt.plot([50,100],[80,100], '+')
+plt.imwrite('new_image.jpg', img)
+plt.show()
